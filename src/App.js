0e856432d4b44d53ebdafe5f702b4cfc323e3ee3
NataliaShchelkunova <>
2021-11-18 17:13:47 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router";
 import RegistrationFormComponent from "./components/RegistrationComponent/RegistrationComponent";
+import AutorisationFormComponent from "./components/autorisationComponent/autorisationComponent";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
-     <RegistrationFormComponent/>
+      <Switch>
+        <Route path="/registration">
+          <RegistrationFormComponent />
+        </Route>
+        <Route path="/autorisation">
+          {" "}
+          <AutorisationFormComponent />{" "}
+        </Route>
+        <Redirect from="/" to="/registration"></Redirect>
+      </Switch>
     </div>
   );
 };
---
+++ src/components/autorisationComponent/autorisationComponent.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Snackbar from "@material-ui/core/Snackbar";
+import logo from "../../icons/Vector.svg";
+import image from "../../icons/image.svg";
+import "./autorisationComponent.scss";
+
+const AutorisationFormComponent = () => {
+  let history = useHistory();
+  const regExLogin = /(\w+).{6}/;
+  const regExPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{1,}$/;
+  const [open, setOpen] = useState(false);
+  const [messages, setMessage] = useState("");
+
+  const checkPass = "Check password";
+  const checkLogin = "Check login";
+
+  const booleanLoginFunction = () => {
+    setOpen(true);
+    setMessage(checkLogin);
+  };
+  const booleanPasswordFunction = () => {
+    setOpen(true);
+    setMessage(checkPass);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const getDataForms = (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    const formLogin = formData.get("Login").trim();
+    const formPassword = formData.get("password");
+    regExLogin.test(formLogin)
+      ? console.log("Login okey")
+      : booleanLoginFunction();
+    regExPassword.test(formPassword)
+      ? console.log("Password okey")
+      : booleanPasswordFunction();
+
+    if (
+      regExLogin.test(formLogin) &&
+      formPassword === formPasswordRepeat &&
+      regExPassword.test(formPassword)
+    ) {
+      axios
+        .post("http://localhost:8000/createUser", {
+          formData,
+        })
+        .then((res) => {
+          formData = new FormData("");
+          registrationPage();
+        });
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <img alt="" src={logo} />
+        <h1 className="header-text">Войти в систему </h1>
+      </header>
+      <div className="body">
+        <div className="image-conainer">
+          <img alt="" src={image} />
+        </div>
+        <div className="registration-conainer">
+          <h1>Войти в систему </h1>
+
+          <div className="form-container">
+            <form onSubmit={getDataForms}>
+              <label>Login: </label>
+              <input type="text" placeholder="Login" name="Login" />
+              <label>Password: </label>
+              <input type="password" placeholder="Password" name="password" />
+              <label>Repeat Password:</label>
+              <div className="button-and-text-conainer">
+                <button>Войти</button>
+                <h4>Зарегистрироваться</h4>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={() => handleClose()}
+        message={messages}
+      />
+    </div>
+  );
+};
+
+export default AutorisationFormComponent;
